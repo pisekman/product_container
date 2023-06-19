dc648807617088956921ad03eeb3031f6434b520
pisekman <pisekman@gmail.com>
2023-06-19 20:32:35 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Double from './components/Double'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div className='vivo-lp' id="vivo_section">
-
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
-
-      </div>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="vivo-lp" id="vivo_section">
+                    <p>test</p>
+                    <p>test</p>
+                    <p>test</p>
+                    <h2>jestem h2</h2>
+                    <section>section</section>
+                    <Double wrapper={1} />
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Double.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Double = ({ wrapper }) => {
+    return <>{wrapper ? <div>Double</div> : 'test'}</>
+}
+
+export default Double
